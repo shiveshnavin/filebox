616a31797a6e3d30cc4cc8c62fface75a007620f
shiveshnavin <shiveshnavin@gmail.com>
2017-12-26 14:55:26 +0530

    Import Libs for Layout

---
+++ app/src/main/java/in/hoptec/filebox/App.java
@@ -0,0 +1,19 @@
+package in.hoptec.filebox;
+
+import android.app.Application;
+
+/**
+ * Created by shivesh on 26/12/17.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+}
+
