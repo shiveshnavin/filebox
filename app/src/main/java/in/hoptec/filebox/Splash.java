a5f1be4e64bb7ccf69c72d0d54089681c0317f27
shiveshnavin <shiveshnavin@gmail.com>
2017-12-30 00:25:16 +0530

    Added Fragment Customization

--- app/src/main/java/in/hoptec/filebox/Splash.java
+++ app/src/main/java/in/hoptec/filebox/Splash.java
@@ -2,6 +2,7 @@ package in.hoptec.filebox;
 
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -23,34 +24,66 @@ public class Splash extends AppCompatActivity {
 
         ImageView mIcDownloadAnimator = (ImageView) findViewById(R.id.logo);
         Drawable drawable = mIcDownloadAnimator.getDrawable();
+
         if (drawable instanceof Animatable) {
             ((Animatable) drawable).start();
         }
-
-
 
         setUpIntro();
 
     }
 
+    ArrayList<Integer> colors;
 
     HelpPagerAdapter pageAdapter;
     public void setUpIntro()
     {
+
+        colors=new ArrayList<>();
+
+        colors.add(R.color.accent);
+        colors.add(R.color.accent);
+        colors.add(R.color.accent);
+
 
         List<Fragment> fragments = getFragments();
         pageAdapter = new HelpPagerAdapter(getSupportFragmentManager(), fragments);
         ViewPager pager =
                 (ViewPager)findViewById(R.id.help_pager);
         pager.setAdapter(pageAdapter);
+
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                utl.l("Pos : "+position+"\n Offset : "+positionOffset+"\n Pix "+positionOffsetPixels);
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                utl.l("Page Sel : "+position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
 
     private List<Fragment> getFragments() {
+
         List<Fragment> fList = new ArrayList<Fragment>();
-        fList.add(Help_Fr0.newInstance("Fragment 1"));
-        fList.add(Help_Fr0.newInstance("Fragment 2"));
-        fList.add(Help_Fr0.newInstance("Fragment 3"));
+
+        fList.add(Help_Fr0.newInstance(colors.get(0),R.drawable.ic_folder_open_48px,false,"No more storming your gallery to find files ," +
+                " Organise your documents and Images in buckets to reach easily ."));
+        fList.add(Help_Fr0.newInstance(colors.get(1),R.drawable.ic_vpn_key_48px,false,"Hide you private Files and Photos with smart lock from Gallery and Spy Apps."));
+        fList.add(Help_Fr0.newInstance(colors.get(2),R.drawable.ic_if_50_cloud_arrow_up_183360,true,"Save important files on Cloud with Easy Sharing !"));
+
         return fList;
      }
 
--- app/src/main/java/in/hoptec/filebox/fragments/Help_Fr0.java
+++ app/src/main/java/in/hoptec/filebox/fragments/Help_Fr0.java
@@ -1,10 +1,19 @@
 package in.hoptec.filebox.fragments;
 
+import android.app.Activity;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DrawableRes;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import in.hoptec.filebox.R;
@@ -14,21 +23,56 @@ import in.hoptec.filebox.R;
  */
 
 public class Help_Fr0 extends Fragment {
+
+    @DrawableRes public int image;
+
     public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
-    public static final Help_Fr0 newInstance(String message)
+
+    public static final Help_Fr0 newInstance(@ColorRes int clr, @DrawableRes int img, boolean isLast, String message)
     {
         Help_Fr0 f = new Help_Fr0();
+
         Bundle bdl = new Bundle(1);
+
         bdl.putString(EXTRA_MESSAGE, message);
+        bdl.putInt("img",img);
+        bdl.putInt("clr",clr);
+        bdl.putBoolean("islast",isLast);
+
         f.setArguments(bdl);
+
         return f;
     }
+
+
+    Activity mActivity;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         String message = getArguments().getString(EXTRA_MESSAGE);
+        int image = getArguments().getInt("img");
+        int clr = getArguments().getInt("clr");
+        boolean islast = getArguments().getBoolean("islast");
+
+        mActivity =getActivity();
+
+
         View v = inflater.inflate(R.layout.fragment_help, container, false);
+
         TextView messageTextView = (TextView)v.findViewById(R.id.desc);
+        AppCompatButton btn=(AppCompatButton)v.findViewById(R.id.next);
+
+        btn.setSupportBackgroundTintList(ContextCompat.getColorStateList(mActivity, clr));
+
+        ImageView img = (ImageView)v.findViewById(R.id.img);
+        img.setImageResource(image);
+        Drawable drawable = img.getDrawable();
+
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+
         messageTextView.setText(message);
         return v;
     }
